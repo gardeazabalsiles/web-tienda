ad24f2b0d63bb411e4d14963cc654ebca7abc9fb
gardeazabalsiles <sabrigardeazabal@gmail.com>
2026-09-06 16:27:31 -0400

    feat: agregar rutas de detalle y carrito

--- src/routes/AppRoutes.tsx
+++ src/routes/AppRoutes.tsx
@@ -1,26 +1,18 @@
-import {
-  BrowserRouter,
-  Route,
-  Routes,
-} from "react-router-dom";
-
-
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import HomePage from "../pages/HomePage";
 import LoginPage from "../pages/auth/LoginPage";
 import RegisterPage from "../pages/auth/RegisterPage";
-
+import ProductDetailPage from "../pages/ProductDetailPage";
+import CartPage from "../pages/CartPage";
 
 function AppRoutes() {
-  return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<HomePage />} />
-        <Route path="/login" element={<LoginPage />} />
-        <Route path="/crear-cuenta" element={<RegisterPage />} />
-      </Routes>
-    </BrowserRouter>
-  );
+  return <BrowserRouter><Routes>
+    <Route path="/" element={<HomePage />} />
+    <Route path="/login" element={<LoginPage />} />
+    <Route path="/crear-cuenta" element={<RegisterPage />} />
+    <Route path="/producto/:id" element={<ProductDetailPage />} />
+    <Route path="/carrito" element={<CartPage />} />
+    <Route path="*" element={<HomePage />} />
+  </Routes></BrowserRouter>;
 }
-
-
 export default AppRoutes;
