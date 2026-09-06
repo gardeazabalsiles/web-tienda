349df8bc0310e2d8acfbebebac85729079005c05
gardeazabalsiles <sabrigardeazabal@gmail.com>
2026-09-06 16:43:02 -0400

    feat: agregar rutas de perfil publicación y chat

--- src/routes/AppRoutes.tsx
+++ src/routes/AppRoutes.tsx
@@ -4,15 +4,8 @@ import LoginPage from "../pages/auth/LoginPage";
 import RegisterPage from "../pages/auth/RegisterPage";
 import ProductDetailPage from "../pages/ProductDetailPage";
 import CartPage from "../pages/CartPage";
+import ProfilePage from "../pages/ProfilePage";
+import PublishProductPage from "../pages/PublishProductPage";
+import ChatPage from "../pages/ChatPage";
 
-function AppRoutes() {
-  return <BrowserRouter><Routes>
-    <Route path="/" element={<HomePage />} />
-    <Route path="/login" element={<LoginPage />} />
-    <Route path="/crear-cuenta" element={<RegisterPage />} />
-    <Route path="/producto/:id" element={<ProductDetailPage />} />
-    <Route path="/carrito" element={<CartPage />} />
-    <Route path="*" element={<HomePage />} />
-  </Routes></BrowserRouter>;
-}
-export default AppRoutes;
+export default function AppRoutes(){return <BrowserRouter><Routes><Route path="/" element={<HomePage/>}/><Route path="/login" element={<LoginPage/>}/><Route path="/crear-cuenta" element={<RegisterPage/>}/><Route path="/producto/:id" element={<ProductDetailPage/>}/><Route path="/carrito" element={<CartPage/>}/><Route path="/perfil" element={<ProfilePage/>}/><Route path="/publicar" element={<PublishProductPage/>}/><Route path="/chat/:userId" element={<ChatPage/>}/><Route path="*" element={<HomePage/>}/></Routes></BrowserRouter>}
